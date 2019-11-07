283a7379021cf23d178f84317ba66cbdc3899320
Rifian Joe Premilenio <55788355+RifianJoe@users.noreply.github.com>
2019-11-07 21:58:43 +0800

    Add files via upload

---
+++ IF1-10118013-Latihan58-TambahKurang/src/if1/pkg10118013/latihan58/tambahkurang/IF110118013Latihan58TambahKurang.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118013.latihan58.tambahkurang;
+
+/**
+ *
+ * @author Rifian
+ * Nama : Rifian Joe Premilenio
+ * Kelas : IF-01
+ * NIM : 10118013
+ */
+public class IF110118013Latihan58TambahKurang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JumlahBilangan hasil = new JumlahBilangan();
+        hasil.tampilHasilJumlah();
+        SelisihBilangan hasil1 = new SelisihBilangan();
+        hasil1.tampilSelisih();
+    }
+    
+}
---
+++ IF1-10118013-Latihan58-TambahKurang/src/if1/pkg10118013/latihan58/tambahkurang/JumlahBilangan.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118013.latihan58.tambahkurang;
+
+/**
+ *
+ * @author Rifian
+ */
+public class JumlahBilangan extends Bilangan{
+    public void tampilHasilJumlah(){
+        System.out.println("Hasil Penjumlahan = " + (getX() + getY()));
+    }
+}
